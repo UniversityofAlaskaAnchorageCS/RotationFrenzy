6994917b688d468d12b82e54ab83cc8952ef3588
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2018-02-28 17:22:52 -0900

    Adding base project configured to run in Android Studio

---
+++ libgdx_prototype/android/src/com/uaa/rotationfrenzy/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.uaa.rotationfrenzy;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.uaa.rotationfrenzy.RotationFrenzy;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RotationFrenzy(), config);
+	}
+}
---
+++ libgdx_prototype/desktop/src/com/uaa/rotationfrenzy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.uaa.rotationfrenzy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.uaa.rotationfrenzy.RotationFrenzy;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RotationFrenzy(), config);
+	}
+}
