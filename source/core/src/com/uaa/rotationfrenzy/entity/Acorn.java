11431355ba30a47257ee6878ece2c5986aba0488
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2018-03-23 22:32:07 -0800

    Add super calls to update and draw.  Create standard constructor that needs to call super constructor

--- source/core/src/com/uaa/rotationfrenzy/entity/Acorn.java
+++ source/core/src/com/uaa/rotationfrenzy/entity/Acorn.java
@@ -3,15 +3,15 @@ package com.uaa.rotationfrenzy.entity;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Acorn extends Rotatable {
-    public Acorn(){
-
+    public Acorn(float axisRotationDelta){
+        super(axisRotationDelta);
     }
 
     public void update(float delta){
-
+        super.update(delta);
     }
 
     public void draw(float delta, SpriteBatch batch){
-
+        super.draw(delta, batch);
     }
 }
