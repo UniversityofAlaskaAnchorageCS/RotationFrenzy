58d9e0eee08739d3c8268c9f321399cb3dec103f
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2018-03-24 17:24:24 -0800

    Add basic super calls to den

--- source/core/src/com/uaa/rotationfrenzy/entity/Den.java
+++ source/core/src/com/uaa/rotationfrenzy/entity/Den.java
@@ -3,15 +3,16 @@ package com.uaa.rotationfrenzy.entity;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Den extends Rotatable {
-    public Den(){
 
+    public Den(float axisRotationDelta){
+        super(axisRotationDelta);
     }
 
     public void update(float delta){
-
+        super.update(delta);
     }
 
     public void draw(float delta, SpriteBatch batch){
-
+        super.draw(delta, batch);
     }
 }
