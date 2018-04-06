53f394f2d6998e81e9494cc5f6e9b5a632b9e0af
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2018-04-06 15:26:56 -0800

    Add basic listeners for touch input and gestures.  Fill in blank print messages for the methods.

--- source/core/src/com/uaa/rotationfrenzy/screen/GameScreen.java
+++ source/core/src/com/uaa/rotationfrenzy/screen/GameScreen.java
@@ -2,16 +2,20 @@ package com.uaa.rotationfrenzy.screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Json;
 import com.uaa.rotationfrenzy.RotationFrenzy;
 import com.uaa.rotationfrenzy.level.Level;
 
 // This is the main game screen that runs, taking user input
-public class GameScreen implements Screen {
+public class GameScreen implements Screen, GestureDetector.GestureListener, InputProcessor {
     private boolean isPaused = false;
     private Level level;
     private final RotationFrenzy game;
@@ -28,6 +32,8 @@ public class GameScreen implements Screen {
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, RotationFrenzy.SCREEN_WIDTH ,RotationFrenzy.SCREEN_HEIGHT);
+
+        setupInput();
     }
 
     @Override
@@ -62,6 +68,19 @@ public class GameScreen implements Screen {
             level.draw(game, delta);
             game.batch.end();
         }
+    }
+
+
+    private void setupInput(){
+
+        //Setup all the Input handling
+        InputMultiplexer im = new InputMultiplexer();
+        GestureDetector gd = new GestureDetector(this);
+        im.addProcessor(gd);
+        im.addProcessor(this);
+
+        //Set the input processor to the multiplexer
+        Gdx.input.setInputProcessor(im);
     }
 
     @Override
@@ -99,4 +118,103 @@ public class GameScreen implements Screen {
         level.dispose();
     }
 
+    /*
+        TOUCH, MOUSE, and KEYBOARD callbacks
+     */
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        System.out.println("Touch Down!");
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        System.out.println("Touch Up!");
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        System.out.println("Touch Dragging it out!");
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(float x, float y, int pointer, int button) {
+        System.out.println("Touch Down! again?");
+        return false;
+    }
+
+    @Override
+    public boolean tap(float x, float y, int count, int button) {
+        System.out.println("Tab my mana");
+        return false;
+    }
+
+    @Override
+    public boolean longPress(float x, float y) {
+        System.out.println("When is this press going to end!");
+        return false;
+    }
+
+    @Override
+    public boolean fling(float velocityX, float velocityY, int button) {
+        System.out.println("It was a short lived relationship.");
+        return false;
+    }
+
+    @Override
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
+        System.out.println("Pan, Peter pan.");
+        return false;
+    }
+
+    @Override
+    public boolean panStop(float x, float y, int pointer, int button) {
+        System.out.println("Ok jokes over.");
+        return false;
+    }
+
+    @Override
+    public boolean zoom(float initialDistance, float distance) {
+        System.out.println("Mazda!");
+        return false;
+    }
+
+    @Override
+    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+        System.out.println("Ouch!");
+        return false;
+    }
+
+    @Override
+    public void pinchStop() {
+        System.out.println("Thank you");
+    }
 }
